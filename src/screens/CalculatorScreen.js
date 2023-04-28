5dfa93a7510e681f7f21b5ce7627117191131711
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-28 09:03:37 -0300

    View separated from Logic

--- src/screens/CalculatorScreen.js
+++ src/screens/CalculatorScreen.js
@@ -5,77 +5,12 @@ import styles from './ScreenStyle';
 import Basica from '../components/Basica/Basica';
 import Trigonometria from '../components/Trigonometria/Trigonometria';
 import Aritmetica from '../components/Aritmetica/Aritmetica';
-
+import  CalculatorLogic  from './CalculatorLogic';
 export default ({ orientation }) => {
   const [currentNumber, setCurrentNumber] = useState('');
   const [lastNumber, setLastNumber] = useState('');
   const [trigonometricFunction, setTrigonometricFunction] = useState('');
   const [aritmetica, setAritmetica] = useState('DEC');
-
-  function calculator() {
-    const splitNumbers = currentNumber.split(' ');
-    let operator = ' ';
-    let actualNumber = 0;
-    let newNumber = parseFloat(splitNumbers[0].toString(10));
-    let index = 1;
-    while (index <= splitNumbers.length - 1) {
-      operator = splitNumbers[index];
-      actualNumber = parseFloat(splitNumbers[index + 1]);
-
-      //Verifica se o número é um número
-      if (!isNaN(actualNumber)) {
-        // Faz ação referente tecla pressionada
-        switch (operator) {
-          case '+':
-            newNumber = newNumber + actualNumber;
-            break;
-          case '-':
-            newNumber = newNumber - actualNumber;
-            break;
-          case 'x':
-            newNumber = newNumber * actualNumber;
-            break;
-          case '/':
-            newNumber = newNumber / actualNumber;
-            break;
-        }
-
-        index = index + 2;
-      } else {
-        return;
-      }
-    }
-
-    newNumber = handleTrigonometric(newNumber);
-
-    setCurrentNumber(handleAritmetica(newNumber));
-  }
-
-  function handleTrigonometric(numberReceive) {
-    switch (trigonometricFunction) {
-      case 'SIN':
-        return (numberReceive = Math.sin(numberReceive));
-      case 'COS':
-        return (numberReceive = Math.cos(numberReceive));
-      case 'TG':
-        return (numberReceive = Math.tan(numberReceive));
-      default:
-        return numberReceive;
-    }
-  }
-
-  function handleAritmetica(numberReceive) {
-    switch (aritmetica) {
-      case 'DEC':
-        return numberReceive.toString();
-      case 'BIN':
-        return numberReceive.toString(2);
-      case 'OCT':
-        return numberReceive.toString(8);
-      case 'HEX':
-        return numberReceive.toString(16);
-    }
-  }
 
   function handleInputBasic(buttonPressed) {
     switch (buttonPressed) {
@@ -96,7 +31,13 @@ export default ({ orientation }) => {
         return;
       case '=':
         setLastNumber(currentNumber + ' = ');
-        calculator();
+        setCurrentNumber(
+          CalculatorLogic(
+            currentNumber,
+            trigonometricFunction,
+            aritmetica
+          )
+        );
         return;
       case '+/-':
         setCurrentNumber((-1 * currentNumber).toString());
